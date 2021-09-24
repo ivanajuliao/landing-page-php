b8ad5c6240861ac81b8cac71c2a255b8c49f2231
Ivana A. Julião <ivana_juliao@yahoo.com.br>
2021-09-24 17:14:23 -0300

    validações

--- lading_page_bd.sql
+++ lading_page_bd.sql
@@ -31,7 +31,7 @@ CREATE TABLE `cliente` (
   `cod_cliente` int(50) NOT NULL,
   `nome` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
-  `mensagem` text NOT NULL
+  `mensagem` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --

8d51d0878f1c0170a432b73f4cce958056db1fd2
Ivana A. Julião <ivana_juliao@yahoo.com.br>
2021-09-25 19:42:29 -0300

    add de comentários

--- conexao.php
+++ conexao.php
@@ -1,18 +1,20 @@
 <?php
 
+    // Define os parâmetros para conexão com o banco
     $hostname = "localhost";
     $bancodedados = "lading_page";
     $usuario = "root";
     $senha = "";
+    // cria uma nova conexão com o BD
     $mysqli = new mysqli($hostname, $usuario, $senha, $bancodedados);
 
+    // verifica status de conexão
     if($mysqli ->connect_errno){
         echo "Falha ao conectar: (" .$mysqli->connect_errno .") " .$mysqli->connect_error;
     }else{
         echo "Mensagem enviada com sucesso!";
+        // Redirecionamento de página
         header('Location: ./main.html');
     }
 
-
-    // $con->close();
 ?>
